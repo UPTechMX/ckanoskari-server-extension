7e7b6c1bbfbeeb64ad5615f776aa3eca4e1bde54
Amilcar Gonzalez <amilkargm@gmail.com>
2022-11-29 08:23:12 -0600

    Adjust

--- server-extension/src/main/java/flyway/wbidp/V1_0_0__add_download_basket.java
+++ server-extension/src/main/java/flyway/wbidp/V1_0_0__add_download_basket.java
@@ -1,27 +1,29 @@
 package flyway.wbidp;
 
-import org.oskari.helpers.AppSetupHelper;
-import org.flywaydb.core.api.migration.BaseJavaMigration;
-import org.flywaydb.core.api.migration.Context;
-
 import java.sql.Connection;
 import java.util.List;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.oskari.helpers.AppSetupHelper;
 
 /**
  * Adds download-basket bundle to default and user views.
  */
 public class V1_0_0__add_download_basket extends BaseJavaMigration {
-    private static final String BUNDLE_ID = "download-basket";
 
-    public void migrate(Context context) throws Exception {
-        Connection connection = context.getConnection();
+  private static final String BUNDLE_ID = "download-basket";
 
-        final List<Long> views = AppSetupHelper.getSetupsForUserAndDefaultType(connection);
-        for(Long viewId : views){
-            if (AppSetupHelper.appContainsBundle(connection, viewId, BUNDLE_ID)) {
-                continue;
-            }
-            AppSetupHelper.addBundleToApp(connection, viewId, BUNDLE_ID);
-        }
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+
+    final List<Long> views = AppSetupHelper.getSetupsForUserAndDefaultType(
+      connection
+    );
+    for (Long viewId : views) {
+      if (AppSetupHelper.appContainsBundle(connection, viewId, BUNDLE_ID)) {
+        continue;
+      }
+      AppSetupHelper.addBundleToApp(connection, viewId, BUNDLE_ID);
     }
+  }
 }
